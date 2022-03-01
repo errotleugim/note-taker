4459219251de5300a7667672b04edac85c1f3a59
Miguel Torre <m.a.torre.f@gmail.com>
2022-03-01 23:42:58 +0100

    delete works now. now why is heroku not working?

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,27 +1,21 @@
 const fs = require("fs");
-//npm package to assign ids, not sure if im using it right
+//npm package to assign ids, not sure if im using it
 const { v4: uuidv4 } = require('uuid');
 
 module.exports = function (app) {
 
 
     app.get("/api/notes", (req, res) => {
-        
-        console.log("\n\nExecuting GET notes request");
-     
+            
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-        
-        console.log("\nGET request - Returning notes data: " + JSON.stringify(data));
-              
+                      
         res.json(data);
     });
 
     app.post("/api/notes", (req, res) => {
     
-        const newNote = request.body;
+        const newNote = req.body;
         
-        console.log("\n\nPOST request - New Note : " + JSON.stringify(newNote));
-
         newNote.id = uuidv4();
 
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -30,8 +24,6 @@ module.exports = function (app) {
 
         fs.writeFileSync('./db/db.json', JSON.stringify(data));
         
-        console.log("\nSuccessfully added new note to 'db.json' file!");
-
         res.json(data);
     });
 
@@ -39,18 +31,14 @@ module.exports = function (app) {
 
     app.delete("/api/notes/:id", (req, res) => {
 
-        let noteId = request.params.id.toString();
+        let noteId = req.params.id.toString();
         
-        console.log(`\n\nDELETE note request for noteId: ${noteId}`);
-
         let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
 
         const newData = data.filter( note => note.id.toString() !== noteId );
 
         fs.writeFileSync('./db/db.json', JSON.stringify(newData));
         
-        console.log(`\nSuccessfully deleted note with id : ${noteId}`);
-
         res.json(newData);
     });
 };
